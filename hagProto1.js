cae2587515ad0c7fc117b7e709202dd514067fa8
Frederick Allsion <ekalb81@gmail.com>
2018-07-12 18:54:39 -0400

    Nathan's Refactor

--- hagProto1.js
+++ hagProto1.js
@@ -1,7 +1,9 @@
 'use strict'; /*jslint node:true*/
 
+ 
+
 module.exports = class HagglesMcGee {
-    constructor(me, counts, values, max_rounds, log){
+    constructor(me, counts, values, max_rounds, log) {
         this.me = me;
         this.counts = counts;
         this.values = values;
@@ -11,195 +13,87 @@ module.exports = class HagglesMcGee {
         for (let j = 0; j < counts.length; j++)
             this.total += counts[j] * values[j];
     }
-    
-
-    offer(o) {
-
-            this.log(`${this.rounds} rounds left`);
-             this.rounds--;
-             this.log(`E31`);
+    offer(offer) {
+        this.log(`${this.rounds} rounds left`);
+        this.rounds--;
+        this.log(`E31`);
 
         // ask for everything if first turn
-            if(this.me == 0 && this.rounds >= 4)
-            {
-                return this.counts;
-            }
-
-
-            if(o) 
-            {
-                  
-
-                    // make array of all items with value to me
-                    let omain = []; 
-                                    
-                    for ( let i = 0; i < this.counts.length; i++)
-                    { 
-
-                        if (this.values[i] > 0)
-                        {
-                            omain = omain.concat(this.counts.slice(i, (i + 1)));     
-                        } 
-                        else 
-                        {
-                            omain[i] = 0;
-                        }
-
-                    }
-                    
-
-                    //calculate total value of offer
-                        let sum = 0;
-                        for (let a = 0; a < o.length; a++)
-                        {
-                        sum += this.values[a] * o[a];
-                        }
-                    // if the sum is equal to the max value accept
-                        if (sum >= this.total)
-                        {
-                        return;
-                        }
-                        
-
-                        else
-                        {
-                            switch(this.rounds + 1)
-                            {
-                                case 5:
-                                {
-                                    return omain;
-                                }
-                                case 4:
-                                {
-                                    let l = omain;
-                                    let extraObjectCounter = 0;
-
-                                    while(extraObjectCounter < 1)
-                                    {
-                                        for(let a = 0; a < l.length; a++)
-                                            {
-                                                if(l[a] > 0 && this.values[a] <= 1)
-                                                {
-                                                    l[a] = l[a] - 1;
-                                                    extraObjectCounter++;
-                                                }
-                                                else
-                                                {
-                                                    extraObjectCounter++;
-                                                }
-                                            }
-                                    }
-                                }
-                                case 3: 
-                                {
-                                    let p = omain; 
-                                    let extraObjectCounter = 0;
-
-                                    while(extraObjectCounter < 2)
-                                    {
-                                        for(let a = 0; a < p.length; a++)
-                                            {
-                                                if(p[a] > 0 && this.values[a] <= 1)
-                                                {
-                                                    p[a] = p[a] - 1;
-                                                    extraObjectCounter++;
-                                                }
-                                                else
-                                                {
-                                                    extraObjectCounter++;
-                                                }
-                                            }
-                                    }
-                                    return p;
-                                }
-                                case 2:
-                                {
-                                    let s = omain; 
-                                    let extraObjectCounter = 0;
-                                
-                                    while(extraObjectCounter < 2)
-                                    {
-                                        for(let a = 0; a < s.length; a++)
-                                            {
-                                                if(s[a] > 0 && this.values[a] <= 2)
-                                                {
-                                                    s[a] = s[a] - 1;
-
-                                                    extraObjectCounter++;
-                                                }
-                                                else if (s[a] > 0 && this.values[a] <= 3)
-                                                {
-                                                    extraObjectCounter++;
-                                                }
-                                                else
-                                                {
-                                                    extraObjectCounter++;
-                                                }
-                                            }
-                                    }
-                                    return s;
-                                }
-                                case 1:
-                                {
-                                    let d = omain;
-                                    let extraObjectCounter = 0;
-
-                                    while(extraObjectCounter < 1)
-                                    {
-                                        for(let a = 0; a < d.length; a++)
-                                            {
-                                                if(d[a] > 0 && this.values[a] <= 1)
-                                                {
-                                                    d[a] = d[a] - 1;
-                                                    extraObjectCounter++;
-                                                }
-                                                else if(d[a] > 0 && this.values[a] <= 2)
-                                                {
-                                                    d[a] = d[a] - 1;
-                                                    extraObjectCounter++;
-                                                }
-                                                else
-                                                {
-                                                    extraObjectCounter++;
-                                                }
-                                            }
-                                    }
-                                    return d;
-                                }
-                                default:
-
-                                return omain;
-                            }
-                                
-                        }
-
-            }
-                    
+        if (this.me == 0 && this.rounds >= 4) {
+            return this.counts;
         }
-};
+        if (offer) {
 
- 
+            // make array of all items with value to me
+            let omain = [];
+            for (let i = 0; i < this.counts.length; i++) {
+                if (this.values[i] > 0) {
+                    omain = omain.concat(this.counts.slice(i, (i + 1)));
+                }
+                else {
+                    omain[i] = 0;
+                }
+            }
+                        //calculate total value of offer
 
+                        let sum = 0;
 
-
-
-
-
-
-
-
-
-             
-                           
-
-
-
-
-
-
-
-
-
+                        for (let i = 0; a < offer.length; i++) {
+                            sum += this.values[i] * offer[i];
+                        }
+            
+                        // if the sum is equal to the max value accept
+            
+                        if (sum >= this.total) {
+            
+                            return;
+            
+                        } else {
+            
+                            switch (this.rounds + 1) {
+                                case 5:
+                                    {
+                                        return omain;
+                                    }
+                                case 4:
+                                    this.build(omain, 1)
+                                    break;
+                                case 3:
+                                    this.build(omain, 1);
+                                    break;
+                                case 2:
+                                    this.build(omain, 2, 3);
+                                    break;
+                                case 1:
+                                    this.build(omain, 1, 2);
+                                    break;
+                            }
+                            return omain;
+                        }
+                    }
+                }
+                build(omain, lessVal, altLessVal) {
+                    let extraObjectCounter = 0;
+                    while (extraObjectCounter < 1) {
+                        for (let i = 0; a < omain.length; i++) {
+                            if (omain[i] > 0 && this.values[i] <= lessVal) {
+                                omain[i] = omain[a] - 1;
+                                extraObjectCounter++;
+                            }
+                            else if ((altLessVal || altLessVal === 0)
+                                && omain[i] > 0
+                                && this.values[i] <= altLessVal
+                            ) {
+                                omain[i] = omain[i] - 1;
+                                extraObjectCounter++;
+                            }
+                            else {
+                                extraObjectCounter++;
+                            }
+                        }
+                    }
+                }
+            };
 
 
 
