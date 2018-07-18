607cfb1942d9914e79e693831a6b5524c5255009
Frederick Allsion <ekalb81@gmail.com>
2018-07-18 19:01:47 -0400

    fixed iterator typo

--- hagProto1.js
+++ hagProto1.js
@@ -1,7 +1,5 @@
 'use strict'; /*jslint node:true*/
 
- 
-
 module.exports = class HagglesMcGee {
     constructor(me, counts, values, max_rounds, log) {
         this.me = me;
@@ -16,15 +14,15 @@ module.exports = class HagglesMcGee {
     offer(offer) {
         this.log(`${this.rounds} rounds left`);
         this.rounds--;
-        this.log(`E31`);
+       
 
-        // ask for everything if first turn
+        // ask for everything the first turn
         if (this.me == 0 && this.rounds >= 4) {
             return this.counts;
         }
         if (offer) {
 
-            // make array of all items with value to me
+            // make array of all items with value > 0
             let omain = [];
             for (let i = 0; i < this.counts.length; i++) {
                 if (this.values[i] > 0) {
@@ -42,14 +40,15 @@ module.exports = class HagglesMcGee {
                             sum += this.values[i] * offer[i];
                         }
             
-                        // if the sum is equal to the max value accept
+                        //  if the sum of the value of objects offered, 
+                        //  is equal to the total value, accept.
             
                         if (sum >= this.total) {
             
                             return;
             
                         } else {
-            
+                        
                             switch (this.rounds + 1) {
                                 case 5:
                                     {
@@ -75,9 +74,9 @@ module.exports = class HagglesMcGee {
                 build(omain, lessVal, altLessVal) {
                     let extraObjectCounter = 0;
                     while (extraObjectCounter < 1) {
-                        for (let i = 0; a < omain.length; i++) {
+                        for (let i = 0; i < omain.length; i++) {
                             if (omain[i] > 0 && this.values[i] <= lessVal) {
-                                omain[i] = omain[a] - 1;
+                                omain[i] = omain[i] - 1;
                                 extraObjectCounter++;
                             }
                             else if ((altLessVal || altLessVal === 0)
